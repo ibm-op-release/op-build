43d21da5f7ce48662469ff1851e4d91fe0f14ba3
hostboot <hostboot@us.ibm.com>
2017-12-01 11:10:27 -0600

    op-build update 12-1-2017
    Changes Included for package hostboot-binaries, branch release-op910:
    9f9dff2 - sannerd - 2017-12-01 - Merge pull request #10 from sannerd/hw120117b.op910
    8c6f320 - Dean Sanner - 2017-12-01 - Update HCODE to hw120117b.op910
    
    Changes Included for package sbe, branch release-op910:
    6d7627a - Nick Klazynski - 2017-12-01 - icache parity errors are not recoverable
    2e1bee3 - Joe McGill - 2017-12-01 - Chip address extension workaround for HW423589 (option2), part1
    c0a8936 - David Kauer - 2017-12-01 - Modify INT FIR configuration settings
    
    Changes Included for package hostboot, branch release-op910:
    c646b78 - Joe McGill - 2017-12-01 - Chip address extension workaround for HW423589 (option2), part2
    42c5c1d - Joe McGill - 2017-12-01 - Chip address extension workaround for HW423589 (option2), part1
    9290e13 - David Kauer - 2017-12-01 - Modify INT FIR configuration settings

--- openpower/package/sbe/sbe.mk
+++ openpower/package/sbe/sbe.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SBE_VERSION ?= 93ed87334ac74185ef2234e557191dd3407618ca
+SBE_VERSION ?= 6d7627af82175f9dd92f4b7dd4b990251229a319
 SBE_SITE ?= $(call github,ibm-op-release,sbe,$(SBE_VERSION))
 
 SBE_LICENSE = Apache-2.0

263eda5e5bec2c7018995a155fa1a5cea05f79e8
jim <jim.yuan@supermicro.com>
2017-12-27 18:27:29 -0800

    update arcconf version to 2.05-22932
    # Conflicts:
    #	openpower/package/arcconf/arcconf.mk

--- openpower/package/arcconf/arcconf.mk
+++ openpower/package/arcconf/arcconf.mk
@@ -4,16 +4,11 @@
 #
 ################################################################################
 
-ARCCONF_VERSION = 2.05-22907
-ARCCONF_SITE = /home/smc/p9dsu/arcconf
-ARCCONF_SITE_METHOD = local
+ARCCONF_VERSION ?= cbf40a719bb7bee5521c2616ddb5bd6bdc7ce1a8
+ARCCONF_SITE ?= $(call github,supermicro,arcconf,$(ARCCONF_VERSION))
+
 ARCCONF_LICENSE = Apache-2.0
 ARCCONF_LICENSE_FILES = COPYING
-
-#define ARCCONF_BUILD_CMDS
-#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) \
-#		INCLUDEDIR="-I." all
-#endef
 
 define ARCCONF_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/arcconf $(TARGET_DIR)/sbin/arcconf

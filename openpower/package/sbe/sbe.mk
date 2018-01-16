c65c20217da5013970f5121fa78c1086d3464cb0
hostboot <hostboot@us.ibm.com>
2018-01-16 14:47:43 -0600

    op-build update 1-16-2018
    Changes Included for package occ, branch release-op910:
    124dbfd - mbroyles - 2018-01-15 - Handle PGPE timeouts as workaround for prolonged droop events
    37d1dc2 - Andres Lugo-Reyes - 2018-01-11 - Prevent WOF reset request on ZZ-L systems
    
    Changes Included for package sbe, branch release-op910:
    99e2fe2 - Sachin Gupta - 2018-01-15 - Update backing build
    051cd68 - Nick Klazynski - 2017-12-28 - Force L1 miss on lqarx
    17320ab - Raja Das - 2017-12-28 - Updated Backing build in customrc to move to Simics DD2
    
    Changes Included for package witherspoon-xml, branch release-op910:
    c1648ec - Erich Hauptli - 2018-01-16 - Populating missing WOF Sort Data.
    ef5f800 - Erich Hauptli - 2018-01-12 - Re-enabling Stop States, WOF, and VDM
    
    Changes Included for package hostboot, branch release-op910:
    cf0a320 - Caleb Palmer - 2018-01-16 - PRD: RCD parity error broadcast mode workaround
    f989ddd - Zane Shelley - 2018-01-16 - PRD: fixed no gard when PLL and CS at same time
    befe7c0 - Ilya Smirnov - 2018-01-15 - Post-process hb timestamps in eSEL.pl
    0d16bd8 - Corey Swenson - 2018-01-15 - Leave MCS acker open until Istep14
    fa6ed5e - Stephen Glancy - 2018-01-15 -  Adds empty training files for HB to mirror
    6e9adac - Prasad Bg Ranganath - 2018-01-15 - p9_pm_reset: check safe mode and, if not, move to Psafe manually
    24b815b - Nick Klazynski - 2018-01-13 - Change workaround for HW430944
    682bc13 - Joe McGill - 2018-01-13 - fix ADU setup for MCD disabled operation
    d4ebcc0 - Louis Stermole - 2018-01-13 - Enable exit_1 mode in FWMS for symbol marks
    8eb8b13 - Louis Stermole - 2018-01-13 - Change ZQ cal fail action to deconfigure MCBIST instead of MCA
    ee039d6 - Prasad Bg Ranganath - 2018-01-13 - Proc VPD:POUND W Version 3-F support
    77155bc - Thi Tran - 2018-01-13 - p9_sbe_hreset HWP
    e5b5d24 - Caleb Palmer - 2018-01-12 - MDIA: Fix overwriting rc from restoreDramRepairs

--- openpower/package/sbe/sbe.mk
+++ openpower/package/sbe/sbe.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SBE_VERSION ?= b6b7bb766f8aef56b63b4c7a91f2f6beeace47d6
+SBE_VERSION ?= 99e2fe2efe0dc47acd1d6286b2a3fa96c64a5018
 SBE_SITE ?= $(call github,ibm-op-release,sbe,$(SBE_VERSION))
 
 SBE_LICENSE = Apache-2.0

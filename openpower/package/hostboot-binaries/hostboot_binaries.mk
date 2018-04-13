27a7fc48b876d02b24f2efe0c35bcebf7a48db14
jim <jim.yuan@supermicro.com>
2018-04-13 15:36:34 -0700

    resolve hostboot_binaries version.

--- openpower/package/hostboot-binaries/hostboot_binaries.mk
+++ openpower/package/hostboot-binaries/hostboot_binaries.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 
-HOSTBOOT_BINARIES_VERSION ?= 10fbf8ec1c777eb185c11634cd9d07e9e6b9dec0
+HOSTBOOT_BINARIES_VERSION ?= 6d09a0a043a7bf22ababf04aeb4765a91c3fa28a
 HOSTBOOT_BINARIES_SITE ?= $(call github,open-power,hostboot-binaries,$(HOSTBOOT_BINARIES_VERSION))
 
 HOSTBOOT_BINARIES_LICENSE = Apache-2.0

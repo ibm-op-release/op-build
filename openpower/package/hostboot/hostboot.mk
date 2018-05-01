58e975cbfb835586affa5b17ae3a1f70f1ac15ba
hostboot <hostboot@us.ibm.com>
2018-05-01 00:40:10 -0500

    op-build update 5-1-2018
    Changes Included for package hostboot, branch release-op910:
    5a3f9ef - Ilya Smirnov - 2018-04-30 - Always Lock Down SBE SEEPROM After SBE Update

--- openpower/package/hostboot/hostboot.mk
+++ openpower/package/hostboot/hostboot.mk
@@ -3,7 +3,7 @@
 # hostboot
 #
 ################################################################################
-HOSTBOOT_VERSION_BRANCH_OP910 ?= ed539397d36558f0fc14a9f00967dae4595b5ff7
+HOSTBOOT_VERSION_BRANCH_OP910 ?= 5a3f9efd23850e6b86b09aaaf18378e9e8790529
 
 HOSTBOOT_VERSION ?= $(HOSTBOOT_VERSION_BRANCH_OP910)
 HOSTBOOT_SITE ?= $(call github,ibm-op-release,hostboot,$(HOSTBOOT_VERSION))

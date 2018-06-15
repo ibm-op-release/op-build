111fd5716c87fb81f1069b1dc58f8d181542005b
hostboot <hostboot@us.ibm.com>
2018-06-15 00:14:35 -0500

    op-build update 6-15-2018
    Changes Included for package hostboot, branch release-op920:
    05e5bff - Prasad Bg Ranganath - 2018-06-15 - SBE:PUTRING: Add callout support for checkword mismatch failure
    8d35703 - spashabk-in - 2018-06-15 - Remove clear(step 3) of reset vectors
    db169d6 - Louis Stermole - 2018-06-14 - Change mss_freq algorithm to deconfigure ports to achieve common DIMM freq
    14d009a - Joe McGill - 2018-06-14 - p9_sbe_common -- mark TP LFIR bits 18:20 recoverable
    cbfc8c2 - Jenny Huynh - 2018-06-14 - Changing NXCQFIR(34) RNG control logic error to checkstop
    f7d423b - Raja Das - 2018-06-14 - Fenced Enabled check along with vdd_pfet_disable_core for scoms
    520c2aa - Greg Still - 2018-06-14 - PM: clear Hcode error injection bit upon PM complex reset
    5caa7eb - Prasad Bg Ranganath - 2018-06-14 - PM_RESCLK: Clear override bits of CACCR in reset flow
    7644b20 - Prasad Bg Ranganath - 2018-06-14 - PM reset: Fix disable resclk procedure
    da0cef6 - Ilya Smirnov - 2018-06-14 - New Global For Console Daemon

--- openpower/package/hostboot/hostboot.mk
+++ openpower/package/hostboot/hostboot.mk
@@ -3,7 +3,7 @@
 # hostboot
 #
 ################################################################################
-HOSTBOOT_VERSION_BRANCH_OP920 ?= 9df095095c4a83a8cda6ee97cd3662e85f5fd0e3
+HOSTBOOT_VERSION_BRANCH_OP920 ?= 05e5bff504e799a3cfba8004cef10bf9dc5f7e35
 
 HOSTBOOT_VERSION ?= $(HOSTBOOT_VERSION_BRANCH_OP920)
 HOSTBOOT_SITE ?= $(call github,ibm-op-release,hostboot,$(HOSTBOOT_VERSION))

11b2588c28b7402ddaa334b9dcbda02550723947
hostboot <hostboot@us.ibm.com>
2018-11-14 00:14:31 -0600

    op-build update 11-14-2018
    Changes Included for package hostboot-binaries, branch release-op920:
    f117d8d - hostboot - 2018-11-09 - UPDATE executables for the NVIDIA gpu and/or ring files for hw110918a.op920
    
    Changes Included for package sbe, branch release-op920:
    421b1db - Prem Shanker Jha - 2018-11-12 - PM: Fixed handling of CME LFIR mask during PM complex reset.
    d50f588 - Dan Crowell - 2018-11-12 - Only save the CME FIR Masks after they have been setup once
    446cf71 - Jenny Huynh - 2018-11-11 - HW471413 Aggressive Uncle: disable ERAT thread sharing
    
    Changes Included for package hcode, branch release-op920:
    14a103e - hostboot - 2018-11-09 - Release tag information updated for hw110918a.op920
    
    Changes Included for package hostboot, branch release-op920:
    f269114 - Jenny Huynh - 2018-11-09 - HW471413 Aggressive Uncle: disable ERAT thread sharing
    
    Signed-off-by: hostboot <hostboot@us.ibm.com>

--- openpower/package/hostboot/hostboot.mk
+++ openpower/package/hostboot/hostboot.mk
@@ -3,7 +3,7 @@
 # hostboot
 #
 ################################################################################
-HOSTBOOT_VERSION_BRANCH_OP920 ?= aed7e4e74ffd68bc7199c1e48cfe186766fc186e
+HOSTBOOT_VERSION_BRANCH_OP920 ?= f2691140aea5899192cac576319962b07ec569cd
 
 HOSTBOOT_VERSION ?= $(HOSTBOOT_VERSION_BRANCH_OP920)
 HOSTBOOT_SITE ?= $(call github,ibm-op-release,hostboot,$(HOSTBOOT_VERSION))

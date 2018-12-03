d758142f70d1049382c95975b99dba8eb27b2c26
hostboot <hostboot@us.ibm.com>
2018-12-03 00:14:16 -0600

    op-build update 12-3-2018
    Changes Included for package hostboot-binaries, branch release-op920:
    d00635a - hostboot - 2018-11-28 - UPDATE executables for the NVIDIA gpu and/or ring files for hw112818a.op920
    eed5487 - hostboot - 2018-11-27 - UPDATE executables for the NVIDIA gpu and/or ring files for hw112718a.op920
    
    Changes Included for package sbe, branch release-op920:
    7aa21f6 - Dean Sanner - 2018-11-26 - Leave scratch valid bits alone to allow HB to query
    
    Changes Included for package hcode, branch release-op920:
    2f9ef53 - hostboot - 2018-11-28 - Release tag information updated for hw112818a.op920
    aa29996 - hostboot - 2018-11-27 - Release tag information updated for hw112718a.op920
    
    Changes Included for package witherspoon-xml, branch release-op920:
    71e7c4f - Erich Hauptli - 2018-11-27 - Default Spectre/Meltdown HW Inits to Kernel mode
    
    Changes Included for package hostboot, branch release-op920:
    3bf7fcf - Corey Swenson - 2018-11-27 - Extend multicast workaround and add tests
    801ca2a - Nick Bofferding - 2018-11-27 - Remove trace buffer flush at end of shutdown path
    eaa2604 - Nick Bofferding - 2018-11-27 - Fix shutdown race condition and task start error in IPMI SEL library
    c112438 - Dan Crowell - 2018-11-27 - Clear out FIR Init parameters after extra PM Resets
    5172324 - Thi Tran - 2018-11-27 - Only call PRD attention handling in resetPMComplex function at runtime.
    9292702 - Dan Crowell - 2018-11-27 - Clear out HOMER attributes and reset PM in PHYP mode
    af26129 - Prem Shanker Jha - 2018-11-27 - PM: Fixed handling of CME LFIR mask during PM complex reset.
    51d2352 - Dan Crowell - 2018-11-27 - Only save the CME FIR Masks after they have been setup once
    9435e74 - Dan Crowell - 2018-11-27 - Reduce ipmi trace spam for pnor hiomap messages
    8e2f7e9 - Mike Baiocchi - 2018-11-27 - Make TOC partion Read-Only in all PNOR Layouts
    
    Signed-off-by: hostboot <hostboot@us.ibm.com>

--- openpower/package/hostboot-binaries/hostboot_binaries.mk
+++ openpower/package/hostboot-binaries/hostboot_binaries.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 
-HOSTBOOT_BINARIES_VERSION ?= f117d8d08d3426b25d2f6b16eb62d1a57c530438
+HOSTBOOT_BINARIES_VERSION ?= d00635acd5ebd36fd3ad950f159bdce7185c5f43
 HOSTBOOT_BINARIES_SITE ?= $(call github,ibm-op-release,hostboot-binaries,$(HOSTBOOT_BINARIES_VERSION))
 
 HOSTBOOT_BINARIES_LICENSE = Apache-2.0

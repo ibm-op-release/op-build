3645f68af3a5fd9946b0d675f0b9ee6cf0c135bc
hostboot <hostboot@us.ibm.com>
2019-01-17 00:15:08 -0600

    op-build update 1-17-2019
    Changes Included for package hostboot, branch release-op920:
    d2c0cb9 - Corey Swenson - 2019-01-16 - Generate error when pnor is not accessed via ipmi
    
    Signed-off-by: hostboot <hostboot@us.ibm.com>

--- openpower/package/hostboot/hostboot.mk
+++ openpower/package/hostboot/hostboot.mk
@@ -3,7 +3,7 @@
 # hostboot
 #
 ################################################################################
-HOSTBOOT_VERSION_BRANCH_OP920 ?= fbb9bf7216bea2ed9a9aba67230d37d7d4b45dc5
+HOSTBOOT_VERSION_BRANCH_OP920 ?= d2c0cb9a339bf88a8f20bac0c181de21cdb36670
 
 HOSTBOOT_VERSION ?= $(HOSTBOOT_VERSION_BRANCH_OP920)
 HOSTBOOT_SITE ?= $(call github,ibm-op-release,hostboot,$(HOSTBOOT_VERSION))
